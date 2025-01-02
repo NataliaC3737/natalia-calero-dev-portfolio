179be8d9f2e33f17f7ef66b410134958b8b3beb6
Natalia <caleronatalia3737@gmail.com>
2025-01-02 22:25:00 +0100

    fix: modification in navbar & added toggle

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,30 +1,49 @@
 import Link from "next/link";
-import { ThemeToggle } from "../atoms/ThemeToggle";
+import { ThemeToggle } from "../atoms/toggles/ThemeToggle";
+import { usePathname } from "next/navigation";
+import LanguageToggle from "../atoms/toggles/LanguageToggle";
 
 export default function Navbar() {
+  const currentPath = usePathname();
+  console.log("currentPath", currentPath);
   return (
-    <nav className="fixed top-0 px-32 py-5 w-full flex items-center justify-between">
-      <Link href={"/"} className="text-brown-coffee font-bagel-fat-one text-2xl">
+    <nav className="fixed top-0 px-32 py-5 w-full flex items-center justify-between border-b border-b-primary">
+      <Link
+        href={"/"}
+        className="text-brown-coffee font-bagel-fat-one text-2xl"
+      >
         COCONAT
       </Link>
       <div className="flex gap-6">
-        <Link href={"/me"} className="text-brown-coffee font-afacad text-lg">
+        <Link
+          href={"/me"}
+          className={`text-brown-coffee font-afacad text-lg ${
+            currentPath === "/me" && "underline"
+          }`}
+        >
           About me
         </Link>
         <Link
           href={"/projects"}
-          className="text-brown-coffee font-afacad text-lg"
+          className={`text-brown-coffee font-afacad text-lg ${
+            currentPath === "/projects" && "underline"
+          }`}
         >
           Projects
         </Link>
         <Link
           href={"/contact"}
-          className="text-brown-coffee font-afacad text-lg"
+          className={`text-brown-coffee font-afacad text-lg ${
+            currentPath === "/contact" && "underline"
+          }`}
         >
           Get in touch
         </Link>
       </div>
+      <div className="flex items-center gap-4">
+      <LanguageToggle/>
       <ThemeToggle />
+      </div>
     </nav>
   );
 }
