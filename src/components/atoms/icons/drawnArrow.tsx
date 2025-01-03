7cca51ef80ae8192e75689e56e9061601dd31925
Natalia <caleronatalia3737@gmail.com>
2025-01-03 01:00:41 +0100

    feat: added some icons & fix: errors on props name

--- src/components/atoms/icons/drawnArrow.tsx
+++ src/components/atoms/icons/drawnArrow.tsx
@@ -10,14 +10,14 @@ export default function DrawnArrow() {
       <path
         d="M45.8505 3.28663C54.9338 1.10639 54.9338 1.10635 48.7571 8.00977"
         stroke="var(--brown-coffee)"
-        stroke-width="3"
-        stroke-linecap="round"
+        strokeWidth="3"
+        strokeLinecap="round"
       />
       <path
         d="M10.2604 99.569C2.63039 79.949 -0.498927 77.1221 3.79684 55.3747C7.6614 35.8104 20.0553 16.7293 47.4753 5.52814"
         stroke="var(--brown-coffee)"
-        stroke-width="3"
-        stroke-linecap="round"
+        strokeWidth="3"
+        strokeLinecap="round"
       />
     </svg>
   );
