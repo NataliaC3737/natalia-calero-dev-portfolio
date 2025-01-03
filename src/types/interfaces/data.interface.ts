3886de1e501e6574c1ed54860dd4f8dfe075fcd0
Natalia <caleronatalia3737@gmail.com>
2025-01-03 17:39:52 +0100

    feat: added more interfaces form and input

--- src/types/interfaces/data.interface.ts
+++ src/types/interfaces/data.interface.ts
@@ -1,7 +1,35 @@
+import { UseFormRegister, FieldValues, FieldErrors } from "react-hook-form";
+
 export interface IExperience {
   dateRange: string;
   jobPosition: string;
   company: string;
   description: string;
   skills: string[];
+}
+export interface IProject {
+  image: string;
+  title: string;
+  description: string;
+  origin: string;
+  dateDeployed: string;
+  links: { web: string; repo: string };
+}
+
+export interface FormData {
+  from: string;
+  to: string;
+  subject: string;
+  message: string;
+}
+
+export interface InputProps {
+  name: string;
+  label?: string;
+  type?: "textField" | "textarea";
+  placeholder?: string;
+  required?: boolean;
+  register: UseFormRegister<any>;
+  error?: FieldErrors<any>;
+  dissabled?: boolean | undefined;
 }
