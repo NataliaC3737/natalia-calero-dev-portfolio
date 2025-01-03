23aeb67e171408ba4b6523799ad4e3c8a17cf050
Natalia <caleronatalia3737@gmail.com>
2025-01-03 01:02:36 +0100

    fix: section layout adding overflow and hidding scrollbar

--- src/components/templates/sections/SectionLayout.tsx
+++ src/components/templates/sections/SectionLayout.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from "react";
 import "./sectionLayout.scss";
 
 export default function SectionLayout({ children }: { children: ReactNode }) {
-  return <section className="section">{children}</section>;
+  return <section className="section flex flex-col gap-32 h-screen overflow-y-scroll">{children}</section>;
 }
